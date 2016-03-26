2646d8000b36f02d3668d48d98363fe41d1961be
vkum29 <vkumar76@sapient.com>
2016-03-26 17:47:42 +0530

    Promise.race handler vs Prmoise.all

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let p2 = new Promise((resolve, reject)=>{
     setTimeout(resolve, 500, '20');
 });
 
-Promise.race([p1,p2]).then((res)=>{
+Promise.all([p1,p2]).then((res)=>{
 	console.log('resolved with ',res);
 },(err)=>{
 	console.error('rejected with ', err)
@@ -19,7 +19,7 @@ let p3 = new Promise((resolve, reject) => {
 	throw new Error('custom error -p3.');
 });
 
-Promise.race([p2,p3]).then((res)=>{
+Promise.all([p2,p3]).then((res)=>{
 	console.log('resolved with ',res);
 },(res)=>{
     console.log('resolved with ',res);
