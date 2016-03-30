bf66ced0b182fa5edb6542a50d239416634e9e4d
vkum29 <vkumar76@sapient.com>
2016-03-30 08:17:51 +0530

    promise.all with rejection handler

--- app.js
+++ app.js
@@ -8,16 +8,20 @@ let p3 = new Promise((resolve, reject)=>{
 	setTimeout(resolve, 1000, '30');
 });
 
+//Executes when all p1,p2,p3 are resolved so will fire after 1ms at least.
 Promise.all([p1,p2,p3]).then((res)=>{
 	console.log('res ',res);
 },(err)=>{
 	console.error('error ', err)
 });
 
+
 let p4 = new Promise((resolve, reject) => {
 	reject('40');
 });
 
+
+//Executes when any rejection occurs so it wont wait for p3 to get resolved.
 Promise.all([p1,p2,p3,p4]).then((res)=>{
 	console.log('res ',res);
 },(err)=>{
