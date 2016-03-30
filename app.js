1cf4872a7ff49499081663549b807bb5f8c2ad02
vkum29 <vkumar76@sapient.com>
2016-03-30 09:05:05 +0530

    promise.race simplified, comments included

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//promise.all
+//promise.race
+//Race executes whenever any one of given promise or iterable is resloved or rejected
 
 'use strict';
 
@@ -7,9 +8,12 @@ let p2 = new Promise((resolve, reject)=>{
     setTimeout(resolve, 500, '20');
 });
 
+
+//Race handling resolve
 Promise.race([p1,p2]).then((res)=>{
 	console.log('resolved with ',res);
 },(err)=>{
+    //will not be called
 	console.error('rejected with ', err)
 });
 
@@ -19,7 +23,9 @@ let p3 = new Promise((resolve, reject) => {
 	throw new Error('custom error -p3.');
 });
 
+//Race handling rejection
 Promise.race([p2,p3]).then((res)=>{
+    //will not be called
 	console.log('resolved with ',res);
 },(res)=>{
     console.log('resolved with ',res);
