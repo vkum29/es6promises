7f448045a4f9af237a8a39cd774acb72a28277d4
vkum29 <vkumar76@sapient.com>
2016-03-30 09:45:50 +0530

    promise.create creation of promise example

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
-//promise.then
-//then returns a promise
-//is used to chain multiple actions
+//promise.creation
 
 'use strict';
 
-let p1 = Promise.resolve(10);
-let p2 = 20;
+
+
+//settled promise state: fulfilled
+let p1 = Promise.resolve('resolve');
+
+//settled promise state: rejected
+let p2 = Promise.reject('rejected');
+
+
+//unsettled promise: pending
 let p3 = new Promise((resolve, reject)=>{
 	setTimeout(resolve, 1000, '30');
 });
 
-//Then handler
-p1.then((res)=>{
-    //handles resolution
-    console.log('response from p1 ',res);
-    return p3;
-}).then((res)=>{
-    console.log('response from p3 ',res);
-    return p2;
-}).then((res)=>{
-    console.log('response from p2 ',res);
-    throw new Error("nothing more to handle");
-}).then((res)=>{
-    console.log('resolve ', res);
-},(err)=>{
-    //handles rejection
-    console.error('recieved ',err);
-});
+console.log(p1);
+
+console.log(p2);
+
+console.log(p3);
