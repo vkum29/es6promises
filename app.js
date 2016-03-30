4989a13ebfb9661cbad7bda9e631cbdb990c3f8e
vkum29 <vkumar76@sapient.com>
2016-03-30 07:16:18 +0530

    thenable example

--- app.js
+++ app.js
@@ -2,16 +2,13 @@
 
 'use strict';
 
-let p1 = Promise.reject("Testing static reject");
+let p1 = new Promise(function(resolve, reject){
+	resolve({then: function(onFullfill, onReject){ console.log("a thenable object in promise."); onReject("then executed");}});
+});
 
 p1.then(function(reason) {
-  // not called
+  console.log("success ",reason);
 }, function(reason) {
-  console.log(reason); // "Testing static reject"
+  console.log("failure ",reason); // "Testing static reject"
 });
 
-Promise.reject(new Error("fail")).then(function(error) {
-  // not called
-}, function(error) {
-  console.log(error); // Stacktrace
-});
