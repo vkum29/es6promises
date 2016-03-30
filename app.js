908430ead8abfffc451ffed7456912a0495eed85
vkum29 <vkumar76@sapient.com>
2016-03-30 08:32:38 +0530

    promise.cach with updated comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//promise.all
+//promise.catch
 
 'use strict';
 
@@ -8,19 +8,26 @@ let p3 = new Promise((resolve, reject)=>{
 	setTimeout(resolve, 1000, '30');
 });
 
+
+
 p1.then((res)=>{
     console.log('response from p1 ',res);
-    throw p3;
+    throw p3; //rejection: Throwing p3 as error
 }).then((res)=>{
+    //will not be called
     console.log('response from p3 ',res);
     return p2;
 },(err)=>{
+    //p3 rejection is handled here
     console.error('error at p3 ',err);
 }).then((res)=>{
+    //res is undefined as nothing was returned from the e3:error handling
     console.log('response from p2 ',res);
     throw new Error("nothing more to handle");
 }).then((res)=>{
+    //will not execute as we have error
     console.log('Nothing to reolve i must quit!');
 }).catch((err)=>{
+    //Error is catught herenode
     console.error('Error unhandled earlier ',err);
 });
