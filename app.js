db6aa3463ebd4feae7f78c418cdcdc1dec694d86
vkum29 <vkumar76@sapient.com>
2016-03-30 08:49:25 +0530

    promise.catch another example with updated comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//promise.all
+//promise.catch
 
 'use strict';
 
@@ -8,19 +8,29 @@ let p3 = new Promise((resolve, reject)=>{
 	setTimeout(resolve, 1000, '30');
 });
 
+//P1 was rejected
 p1.then((res)=>{
+    //will not be called
     console.log('response from p1 ',res);
     return p3;
 }).then((res)=>{
+    //will not be called
     console.log('response from p3 ',res);
     return p2;
 },(err)=>{
+    //error is handled here
     console.error('error at p3 ',err);
 }).then((res)=>{
+    //executes as error is handled earlier
     console.log('response from p2 ',res);
     throw new Error("nothing more to handle");
 }).then((res)=>{
+    //will not be called
     console.log('Nothing to reolve i must quit!');
 }).catch((err)=>{
+    //executed as error is handled
     console.error('Error unhandled earlier ',err);
 });
+
+
+//then(undefined, (err)=>{}) is equivalent to catch((err)=>{})
