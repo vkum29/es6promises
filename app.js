52fc968de41eaf468664ee632986e2ff9bdb4af1
vkum29 <vkumar76@sapient.com>
2016-03-30 09:11:17 +0530

    promise.race compared with promise.all simplified, comments included

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //promise.all
+//This compare the Promise.race taggeed in promise.race-01
 
 'use strict';
 
@@ -7,6 +8,7 @@ let p2 = new Promise((resolve, reject)=>{
     setTimeout(resolve, 500, '20');
 });
 
+//executes once both p1 and p2 is resolved
 Promise.all([p1,p2]).then((res)=>{
 	console.log('resolved with ',res);
 },(err)=>{
@@ -18,7 +20,7 @@ Promise.all([p1,p2]).then((res)=>{
 let p3 = new Promise((resolve, reject) => {
 	throw new Error('custom error -p3.');
 });
-
+//excutes on first rejection like race
 Promise.all([p2,p3]).then((res)=>{
 	console.log('resolved with ',res);
 },(res)=>{
