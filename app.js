edc34d7daf5aa7d2419582ea9407bed04e5971bf
vkum29 <vkumar76@sapient.com>
2016-03-30 08:55:26 +0530

    promise.catch rejection more simplified with updated comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//promise.all
+//promise.catch
 
 'use strict';
 
@@ -8,17 +8,23 @@ let p3 = new Promise((resolve, reject)=>{
 	setTimeout(resolve, 1000, '30');
 });
 
+//p1 is rejected
 p1.then((res)=>{
+    //will not run
     console.log('response from p1 ',res);
     return p3;
 }).then((res)=>{
+    //will not run
     console.log('response from p3 ',res);
     return p2;
 }).then((res)=>{
+    //will not run
     console.log('response from p2 ',res);
     throw new Error("nothing more to handle");
 }).catch((err)=>{
+    //rejection will be handled here
     console.error('Error unhandled earlier ',err);
 }).then((res)=>{
+    //As error is handled this will run
     console.log('Nothing to resolve i must quit!');
 });
